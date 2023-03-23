7a13dd34dbaffc401081ffa2a632e027a85f3ad9
A Tunc <106016458+aatunc@users.noreply.github.com>
2023-03-23 13:42:34 +0100

    Initial commit

---
+++ alse_if.py
@@ -0,0 +1,10 @@
+action = int (input("Geben Sie Action: "))
+
+if action == 1:
+    print ("Action 1 wählen..")
+elif action == 2:
+    print ("Action 2 wählen.." )
+elif action == 3:
+    print ("Action 3 wählen")
+else:
+    print ("Action ist ungültig..")
